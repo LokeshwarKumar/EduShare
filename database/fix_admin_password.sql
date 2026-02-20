9156bbd55ec2752765bcf9ed7cbef33f3137cba9
LokeshwarKumar <thotalokeshwarkumar@gmail.com>
2026-02-20 16:29:40 +0530

    removed data files from project

--- database/fix_admin_password.sql
+++
@@ -1,9 +0,0 @@
-USE edumat_db;
-
-UPDATE users 
-SET password = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'
-WHERE username = 'admin';
-
-SELECT username, email, password, first_name, last_name
-FROM users 
-WHERE username = 'admin';
